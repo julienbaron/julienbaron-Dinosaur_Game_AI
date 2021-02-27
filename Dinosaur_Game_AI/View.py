ce8ff44f625166df5c77228bfb2e6c479919106d
julienbaron <33124169+julienbaron@users.noreply.github.com>
2021-02-27 11:58:41 +0100

    Add View

---
+++ Dinosaur_Game_AI/View.py
@@ -0,0 +1,14 @@
+import pygame as pg 
+
+class View(object):
+    """description of class"""
+    @staticmethod
+    def DrawWindow(birds, dinosaurs, base, win):
+        for bird in birds:
+            bird.draw(win)
+        for dinosaur in dinosaurs:
+            dinosaur.draw(win)
+        base.draw(win)
+        pg.display.update()
+
+
